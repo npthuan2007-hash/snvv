f1f651bd6d6b3553bf9be4dc0ee1e2969e27176a
npthuan2007-hash <npthuan2007@gmail.com>
2026-02-01 22:29:22 +0700

    script_v2.js

--- script_v2.js
+++ script_v2.js
@@ -1,25 +1,35 @@
+console.log("JS ĐÃ CHẠY"); // 👈 DÒNG TEST
+
 let count = 3;
 const cd = document.getElementById("countdown");
 
 const timer = setInterval(() => {
   cd.innerText = count;
   count--;
+
   if (count < 0) {
     clearInterval(timer);
     cd.style.display = "none";
-    startFireworks();
+
+    // kiểm tra có pháo hoa không
+    if (typeof startFireworks === "function") {
+      startFireworks();
+    }
+
     setTimeout(showFriend, 5000);
   }
 }, 1000);
 
 function showFriend() {
+  const img = document.getElementById("friend");
+  const btn = document.getElementById("letterBtn");
+
+  img.style.display = "block";
+  btn.style.display = "block";
+}
+
 document.getElementById("letterBtn").onclick = () => {
-  // Ẩn ảnh + nút
   document.getElementById("friend").style.display = "none";
   document.getElementById("letterBtn").style.display = "none";
-
-  // Hiện thư
   document.getElementById("envelope").style.display = "block";
 };
-
-
