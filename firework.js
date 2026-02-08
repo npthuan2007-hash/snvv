9d74d75c28b83bccb8c58d21a7d3b76fc8524187
npthuan2007-hash <npthuan2007@gmail.com>
2026-02-08 13:09:16 +0700

    firework.js

--- firework.js
+++ firework.js
@@ -2,14 +2,13 @@ function startFireworks() {
   for (let i = 0; i < 30; i++) {
     const f = document.createElement("div");
     f.style.position = "fixed";
-    f.style.left = Math.random() * 100 + "%";
-    f.style.top = Math.random() * 100 + "%";
+    f.style.left = Math.random()*100 + "%";
+    f.style.top = Math.random()*100 + "%";
     f.style.width = "6px";
     f.style.height = "6px";
-    f.style.background =
-      `hsl(${Math.random() * 360},100%,50%)`;
+    f.style.background = `hsl(${Math.random()*360},100%,50%)`;
     f.style.borderRadius = "50%";
     document.body.appendChild(f);
-    setTimeout(() => f.remove(), 2000);
+    setTimeout(()=>f.remove(),2000);
   }
 }
