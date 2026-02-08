94c65d14955fff548832e465284f1ef226d3e4e2
npthuan2007-hash <npthuan2007@gmail.com>
2026-02-08 13:08:54 +0700

    script.js

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let count = 3;
-  const cd = document.getElementById("countdown");
+let count = 3;
+const cd = document.getElementById("countdown");
 
-  const timer = setInterval(() => {
-    cd.innerText = count;
-    count--;
+const timer = setInterval(() => {
+  cd.innerText = count;
+  count--;
+  if (count < 0) {
+    clearInterval(timer);
+    cd.style.display = "none";
+    startFireworks();
+    setTimeout(showFriend, 5000);
+  }
+}, 1000);
 
-    if (count < 0) {
-      clearInterval(timer);
-      cd.style.display = "none";
-      startFireworks();
+function showFriend() {
+  document.getElementById("friend").style.display = "block";
+  document.getElementById("letterBtn").style.display = "block";
+}
 
-      setTimeout(() => {
-        document.getElementById("friend").style.display = "block";
-        document.getElementById("letterBtn").style.display = "block";
-      }, 2000);
-    }
-  }, 1000);
-
-  document.getElementById("letterBtn").onclick = () => {
-    document.getElementById("friend").style.display = "none";
-    document.getElementById("letterBtn").style.display = "none";
-    document.getElementById("envelope").style.display = "block";
-  };
-});
+document.getElementById("letterBtn").onclick = () => {
+  document.getElementById("envelope").style.display = "block";
+};
